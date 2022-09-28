e709825c5c7382c58014de4680ba1543ca719e4f
Tval0 <andriagamer264@gmail.com>
2022-09-28 22:23:11 +0400

    day 3

---
+++ day 3/homework3.py
@@ -0,0 +1,27 @@
+
+
+
+your_name = input("enter your name: " )
+your_surname = input("enter your surname: " )
+your_age = input("enter your age: " )
+your_location = input("enter your location: " )
+your_hobby = input("enter your hobby: " )
+your_height=input("enter your height ")
+
+print("my name is " + your_name, "my surename is " + your_surname,"i am " + your_age, "years old","i am from " + your_location , "what is your hobby " + your_hobby,"i am " + your_height,"cm tall")
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day 3/lesson3.py
@@ -0,0 +1,6 @@
+my_name="ანდრია"
+my_surname="თვალოქროშვილი"
+my_age="14"
+my_sentence="გამარჯობა ჩემი სახელია და გვარია{0}{1} და მე ვარ {2}წლის ".format(my_name,my_surname,my_age)
+
+
