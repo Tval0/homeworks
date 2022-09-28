2fff04184bdffa55da7b677107d7252e2545c0c6
Tval0 <andriagamer264@gmail.com>
2022-09-28 23:22:57 +0400

    first commit

--- day 3/homework3.py
+++ day 3/homework3.py
@@ -1,16 +1,27 @@
 
 
 
-your_name = input("enter your name: " )
-your_surname = input("enter your surname: " )
-your_age = input("enter your age: " )
-your_location = input("enter your location: " )
-your_hobby = input("enter your hobby: " )
-your_height=input("enter your height ")
+your_name = str(input("enter your name: " ))
+your_surname = str(input("enter your surname: " ))
+your_age = int(input("enter your age: " ))
+your_location = str(input("enter your location: " ))
+your_hobby = str(input("enter your hobby: " ))
+your_height= int(input("enter your height "))
+question= str(input("do you have pet "))
 
-print("my name is " + your_name, "my surename is " + your_surname,"i am " + your_age, "years old","i am from " + your_location , "what is your hobby " + your_hobby,"i am " + your_height,"cm tall")
+
+if question=="yes":
+    print("what kind of pet")
+    x=str(input())
+
+    print("my name is " , your_name, "my surename is " ,your_surname,"i am " , your_age, "years old","i am from ",  your_location , "my hobby is ",  your_hobby,"i am ",  your_height,"cm tall",
+     " and i have " ,x)
 
 
+
+
+elif question=="no":  
+    print("my name is " , your_name, "my surename is " , your_surname,"i am " , your_age, "years old","i am from " , your_location , "my hobby is " , your_hobby,"and i am " , your_height,"cm tall")
  
 
 
