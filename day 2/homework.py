f2add5a64bf2ddd48f380054dd7168d301cc1ff5
Tval0 <andriagamer264@gmail.com>
2022-09-28 19:19:26 +0400

    day 2

---
+++ day 2/homework.py
@@ -0,0 +1,14 @@
+name = "ანდრია"
+
+print(len(name))
+
+fulname = "ანდრია თვალოქროშვილი"
+
+print(len(fulname))
+
+my_sentence = "აქ არ ვიცი რა დავწერო"
+
+print(my_sentence)
+
+if 210<1506:
+    print("lol is better than dota")
